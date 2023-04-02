c62434d3ab5f8d8ded3fbb8800b011f0018335a2
whata <whatacrap@gmail.com>
2023-04-02 20:17:45 +0300

    Правки version 3.0

--- src/main/java/ru/home/homework/lesson6/oop/animals/Animal.java
+++ src/main/java/ru/home/homework/lesson6/oop/animals/Animal.java
@@ -2,11 +2,16 @@ package ru.home.homework.lesson6.oop.animals;
 
 public abstract class Animal {
     String name;
+
     public abstract void run(int runLength);
+
     public abstract void swim(int swimLength);
+
     public boolean isAnimal() {
         return true;
     }
+
     public abstract boolean isCat();
+
     public abstract boolean isDog();
 }
